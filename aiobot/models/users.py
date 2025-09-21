c0b8c0752a05ad53648c33dab4a0b2e4472b51af
AbdusalomovIbroxim <ibrohimlololowka@gmail.com>
2025-09-21 16:14:07 +0500

    update users model

--- aiobot/models/users.py
+++ aiobot/models/users.py
@@ -9,7 +9,7 @@ from aiobot.database import Base, db
 # auto create id
 class Users(Base):
     __tablename__ = "users"
-    user_id = Column(BigInteger(50), unique=True)
+    user_id = Column(BigInteger, unique=True)
     full_name = Column(String(30))
     phone_number = Column(String(30))
     lang = Column(String(2))
