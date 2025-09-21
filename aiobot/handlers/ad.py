1b42f126215ec629de015d432ac83eceb34ca7d5
AbdusalomovIbroxim <ibrohimlololowka@gmail.com>
2025-09-21 13:07:09 +0500

    fix bag with reply keyboards

--- aiobot/handlers/ad.py
+++ aiobot/handlers/ad.py
@@ -129,7 +129,7 @@ async def ad_price(message: Message, state: FSMContext):
 async def ad_size_category(message: Message, state: FSMContext):
     await state.update_data(size_category=message.text)
     user = await Users.get(user_id=message.from_user.id)
-    await message.answer(TEXTS["ad_size"][user.lang], clothing_size_keyboard())
+    await message.answer(TEXTS["ad_size"][user.lang], reply_markup=clothing_size_keyboard())
     await state.set_state(AdForm.size)
 
 
@@ -145,7 +145,7 @@ async def ad_size(message: Message, state: FSMContext):
 async def ad_condition(message: Message, state: FSMContext):
     await state.update_data(condition=message.text)
     user = await Users.get(user_id=message.from_user.id)
-    await message.answer(TEXTS["ad_photos"][user.lang], photos_keyboard())
+    await message.answer(TEXTS["ad_photos"][user.lang], reply_markup=photos_keyboard())
     await state.set_state(AdForm.photos)
 
 
