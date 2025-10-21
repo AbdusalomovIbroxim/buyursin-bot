e3705525d65591c8aa27765f83afa96b7b035c1b
AbdusalomovIbroxim <ibrohimlololowka@gmail.com>
2025-10-22 01:20:12 +0500

    FIX

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ load_dotenv()
 
 
 class Config:
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-    DB_NAME = os.getenv("DB_NAME")
-    DB_HOST = os.getenv("DB_HOST")
+    DB_USER = os.getenv("POSTGRES_USER")
+    DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    DB_NAME = os.getenv("POSTGRES_NAME")
+    DB_HOST = os.getenv("POSTGRES_HOST")
     # DB_CONFIG = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/postgres"
     # DB_CONFIG = f"postgresql+asyncpg://postgres:0@localhost/postgres"
     # DB_CONFIG = f"sqlite:///db.sqlite"
